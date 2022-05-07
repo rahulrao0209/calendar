d14a62b36e9fa3c232aebf0f261fe1fc28b9aeba
unknown <rahulrao0209@gmail.com>
2022-05-07 14:47:46 +0530

    updating logic to render mini calendar

--- src/components/CalendarMini/CalendarMini.tsx
+++ src/components/CalendarMini/CalendarMini.tsx
@@ -1,19 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { IoCaretBack, IoCaretForward } from "react-icons/io5";
-import type { Data } from "src/types/interfaces";
+import type { Data, monthData } from "src/types/interfaces";
+import { getMonthData } from "../../utils/getMonthData";
 import "./CalendarMini.scss";
 
 export const CalendarSmall = ({ data }: Data) => {
+
+    const [miniData, setMiniData] = useState<monthData[]>([]);
+    const [month, setMonth] = useState(new Date().getMonth());
+
+    const handleNext = () => {
+        console.log("SET MONTH");
+        setMonth(month + 1);
+    };
+
+    const handlePrev = () => {
+        console.log("SET MONTH");
+        setMonth(month - 1);
+    };
+
+    useEffect(() => {
+      setMiniData(getMonthData(month))
+    }, [month]);
+
+    useEffect(() => {
+        setMiniData(data);
+    }, [data]);
+
     return (
         <div className="calendar-mini">
             
             <div className="calendar-mini__nav">
-            <div className="calendar-mini__month">{data[data.length - 1]?.monthName} {data[data.length - 1]?.year}</div>
+            <div className="calendar-mini__month">{miniData[miniData.length - 1]?.monthName} {miniData[miniData.length - 1]?.year}</div>
                 <div className="calendar-mini__nav-controls">
-                    <span className="nav__controls--previous">
+                    <span className="nav__controls--previous" onClick={handlePrev}>
                         <IoCaretBack className="icon--prev" />
                     </span>
-                    <span className="nav__controls--next">
+                    <span className="nav__controls--next" onClick={handleNext}>
                         <IoCaretForward className="icon--next"/>
                     </span>
                 </div>
@@ -29,7 +52,7 @@ export const CalendarSmall = ({ data }: Data) => {
                 <span>S</span>
             </div>
             <div className="calendar-mini__calendar">
-            {data.map((item, index) => {
+            {miniData.map((item, index) => {
              return (
                <div className="calendar-mini__cell" key={index}>
                  <span className="calendar-mini__month">{item.day}</span>
