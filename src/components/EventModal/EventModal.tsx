33b083240126e2c35148cf36b85a13308dcd2a42
unknown <rahulrao0209@gmail.com>
2022-05-14 10:56:59 +0530

    adding fields to the event modals

--- src/components/EventModal/EventModal.tsx
+++ src/components/EventModal/EventModal.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import type { EventModalType } from "../../types/types";
+import { MdOutlineEvent } from "react-icons/md";
+import { BsPencil } from "react-icons/bs";
 import "./EventModal.scss";
 
 export const EventModal:  ({ show, date, setShowModal }: EventModalType) => JSX.Element | null = ({ show, date, setShowModal }: EventModalType) => {
@@ -8,15 +10,28 @@ export const EventModal:  ({ show, date, setShowModal }: EventModalType) => JSX.
   
   return show ? (
     <form className="event-modal">
+      
       <div className="event-modal__header">
         <div className="event-modal__menu-btn">
           <span></span>
         </div>
        <div className="event-modal__close-btn" onClick={handleModal}>
           <span></span>
-        </div>
-        <div></div>
-      </div>
+       </div>
+     </div>
+
+     <div className="event-modal__form">
+         <div className="event-modal__field">
+           <MdOutlineEvent className="event-modal__icon" />
+           <input type="text" name="title" placeholder="Add Title"/>
+         </div>
+
+         <div className="event-modal__field">
+           <BsPencil className="event-modal__icon" />
+           <input type="text" name="description" placeholder="Add Description"/>
+         </div>
+     </div>
+    
     </form>
     ) : null 
 }
