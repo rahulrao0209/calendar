31d5633273f03a9f3add20c2f98e1bc44aec291c
unknown <rahulrao0209@gmail.com>
2022-06-23 13:04:44 +0530

    adding calendar image for landing page | updating the styling for the landing page

--- src/components/LandingBody/LandingBody.tsx
+++ src/components/LandingBody/LandingBody.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { Signup } from "../Signup/Signup";
 import { Login } from "../Login/Login";
+import calendarImage from "../../../public/images/calendar.jpg";
 import "./LandingBody.scss";
 
 export const LandingBody = () => {
     return (
       <section className="landing-body">
         <div className="landing-body__image">
-           {/* Add an image here */}
+           <img src={calendarImage} alt="calendar"/>
         </div>
         <div className="landing-body__form">
           <Routes>
@@ -16,6 +17,9 @@ export const LandingBody = () => {
             <Route path="/login" element={<Login />}/>
           </Routes>
         </div>
+        <footer>
+          Copyright &copy; 2022
+        </footer>
       </section>
     )
 }
