6b7743e0fe39c38e40a5dd2f79ab413738e1a988
unknown <rahulrao0209@gmail.com>
2022-06-25 14:26:23 +0530

    initializing firebase for authentication

---
+++ src/firebase/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAxSrbosYc19nIzcwKVwW2KyS67YvSsYcg",
+  authDomain: "calendar-c03b4.firebaseapp.com",
+  projectId: "calendar-c03b4",
+  storageBucket: "calendar-c03b4.appspot.com",
+  messagingSenderId: "913327772781",
+  appId: "1:913327772781:web:6ac962865a2df31a79db64"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
